e5591e37fb19c9e264a9dee08dd3a1965afd6f54
Sudo Rion <gakkoyou41@gmail.com>
2023-09-05 00:54:07 +0900

    change socket_event

--- chatapp/socket_event/index.js
+++ chatapp/socket_event/index.js
@@ -14,5 +14,40 @@ export default (io, socket) => {
     io.sockets.emit("publishEvent", data)
   })
 
+  const voteName = {};
+
+  io.on('connection', (socket) => {
+    socket.on("addVoteName", (name) => {
+      const playerNumber = Object.keys(voteName).length + 1;
+      const playerName = `player${playerNumber}`;
+      voteName[playerName] = name
+
+      socket.emit("sendPlayerName", playerName);
+    })
+  });
+  
+
+  let count_yonekura = 0;
+  let count_sudo = 0;
+  let count_morita = 0;
+  let count_hara = 0;
+  
+  socket.on("voteEvent", (name) => {
+    if (name == "yonekura")
+      count_yonekura++
+    if (name == "sudo")
+      count_sudo++
+    if (name == "morita")
+      count_morita++
+    if (name == "hara")
+      count_hara++
+
+    let total_count = count_yonekura + count_sudo + count_morita + count_hara;
+
+    if (total_count == 3)
+    maxValue = max(count_yonekura,count_sudo,count_morita,count_hara)
+    io.sockets.emit("voteReturn", ansName)
+  });
+
   
 }
