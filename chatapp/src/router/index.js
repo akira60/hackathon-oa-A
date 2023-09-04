808c4efebe40885c89295b35ea8eaf6e936d6d13
Sudo Rion <gakkoyou41@gmail.com>
2023-09-05 01:15:14 +0900

    test

--- chatapp/src/router/index.js
+++ chatapp/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Chat from "../components/Chat.vue"
 import Login from "../components/Login.vue"
+import { vModelText } from "vue"
 
 const router = createRouter({
   history: createWebHistory(),
@@ -20,7 +21,7 @@ const router = createRouter({
           next({ name:"login" })
         }
       },
-    }
+    },
   ],
 })
 
